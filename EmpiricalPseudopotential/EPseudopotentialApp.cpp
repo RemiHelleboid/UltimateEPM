9cd9baceb7af250754b7dc5772f33e622a870bfa
Adrian Roman <aroman_ro@yahoo.com>
2018-09-05 20:14:19 +0300

    Call OnInit from application base class

--- EmpiricalPseudopotential/EPseudopotentialApp.cpp
+++ EmpiricalPseudopotential/EPseudopotentialApp.cpp
@@ -7,6 +7,9 @@ wxIMPLEMENT_APP(EPseudopotentialApp);
 
 bool EPseudopotentialApp::OnInit()
 {
+	if (!wxApp::OnInit())
+		return false;
+	
 	frame = new EPseudopotentialFrame("EPseudopotential", wxPoint(50, 50), wxSize(1024, 800));
 	frame->Show(true);
 
