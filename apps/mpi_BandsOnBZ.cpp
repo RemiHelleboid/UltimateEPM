8fcabf889eb58b56e86f8a2a60bec8e4c3ea7e38
RemiHelleboid <remi.helleboid@gmail.com>
2022-09-14 14:18:27 +0200

    Small changes in log

--- apps/mpi_BandsOnBZ.cpp
+++ apps/mpi_BandsOnBZ.cpp
@@ -49,6 +49,7 @@ typedef struct vector_k {
 } vector_k;
 
 int main(int argc, char* argv[]) {
+    std::cout << "Starting MPI version of BandsOnBZ" << std::endl;
     // Initialize the MPI environment.
     MPI_Status status;
 
@@ -57,7 +58,6 @@ int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &number_processes);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
-
 
     TCLAP::CmdLine               cmd("EPP PROGRAM. COMPUTE BAND STRUCTURE ON A BZ MESH.", ' ', "1.0");
     TCLAP::ValueArg<std::string> arg_mesh_file("f", "meshfile", "Name to print", true, "bz.msh", "string");
@@ -112,12 +112,13 @@ int main(int argc, char* argv[]) {
 
     std::vector<vector_k> all_k_vectors;
     long                  number_k_vectors = 0;
-    const std::string mesh_filename = arg_mesh_file.getValue();
-    bz_mesh_points    my_mesh(mesh_filename);
+    const std::string     mesh_filename    = arg_mesh_file.getValue();
+    bz_mesh_points        my_mesh(mesh_filename);
     if (process_rank == MASTER) {
         my_mesh.read_mesh();
         std::vector<Vector3D<double>>& mesh_k_points = my_mesh.get_kpoints();
         number_k_vectors                             = mesh_k_points.size();
+        std::cout << "Total number of k points: " << number_k_vectors << std::endl;
         all_k_vectors.resize(mesh_k_points.size());
         for (size_t i = 0; i < mesh_k_points.size(); i++) {
             all_k_vectors[i].set_k(mesh_k_points[i].X, mesh_k_points[i].Y, mesh_k_points[i].Z);
